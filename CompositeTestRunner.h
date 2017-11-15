b450812cf2e70d3869c8a16daf1f3d99851665c1
David Morse <morse012@umn.edu>
2017-11-15 12:42:18 -0600

    Updating documentation in CompositeTestRunner

--- CompositeTestRunner.h
+++ CompositeTestRunner.h
@@ -49,10 +49,13 @@ public:
    void addChild(TestRunner* childPtr);
 
    /**
-   * Add a TestRunner as a child, accept ownership, and set file prefix.
+   * Add a TestRunner as a child, accept ownership, and initialize filePrefix.
    *
    * Children added by this method are owned by the parent CompositeTestRunner,
-   * and so are destroyed by its destructor.  
+   * and so are destroyed by its destructor.  The file prefix argument should
+   * normally be a path for a particular child defined relative to any common 
+   * prefix used by all tests in this composite. The common prefix can then be
+   * prepended by calling addFilePrefix at run time. 
    *
    * \param childPtr  pointer to child TestRunner
    * \param prefix  prefix to append to file names in all descendants
@@ -61,6 +64,11 @@ public:
 
    /**
    * Prepend argument prefix to existing filePrefix.
+   *
+   * This function also prepends prefix to all children.  If this function
+   * is called at run-time for the highest level composite in a hierarchy,
+   * the prefix is thus propagated to all TestRunners in the hierarchy, and
+   * thus also used in the methods of UnitTest that are used to open files.
    *
    * \param prefix string to prepend to existing filePrefix.
    */
@@ -158,20 +166,23 @@ int CompositeTestRunner::run()
       CompositeClass () {
 
 /**
-* Macro to add a UnitTest subclass to a CompositeTestRunner constructor.
+* Macro to add a UnitTestRunner to a CompositeTestRunner constructor.
 *
-* The parameter UnitTestClass is the name of the UnitTest subclass.
-* An instance of corresponding UnitTestRunner is instantiated internally.
+* The parameter UnitTestClass is the name of a UnitTest subclass. 
+* An instance of the corresponding TEST_RUNNER(UnitTestClass) class
+* is created and added to this CompositeTestRunner.
 *
 */
 #define TEST_COMPOSITE_ADD_UNIT(UnitTestClass) \
          addChild(new TEST_RUNNER(UnitTestClass)); 
 
 /**
-* Macro to add a UnitTest subclass to a CompositeTestRunner constructor.
+* Macro to add a TestRunner to a CompositeTestRunner constructor.
 *
-* The parameter UnitTestClass is the name of the UnitTest subclass.
-* An instance of corresponding UnitTestRunner is instantiated internally.
+* The parameter TestRunner is the name of a TestRunner subclass. An 
+* instance of corresponding TestRunner subclass is instantiated and
+* added to this CompositeTestComposite, and its filePrefix is set
+* to a specifiec value prefix. 
 *
 */
 #define TEST_COMPOSITE_ADD_CHILD(TestRunner, Prefix) \
